a54b7ab61847ad81fc8d6506427c19c3d48200ae
vyshnavi490 <161860867+vyshnavi490@users.noreply.github.com>
2024-09-06 11:59:49 +0530

    code changes

--- src/test/java/base/DriverManager.java
+++ src/test/java/base/DriverManager.java
@@ -14,18 +14,12 @@ public class DriverManager {
 	public static ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
 
 	public void createWebDriverInstance(String browserName, String hubURL) throws MalformedURLException {
-		if (hubURL.isBlank() || hubURL.isEmpty()) {
+		
 			if (browserName.equalsIgnoreCase("chrome")) {
 				webDriver.set(new ChromeDriver());
 			} else if (browserName.equalsIgnoreCase("firefox")) {
 				webDriver.set(new FirefoxDriver());
 			}
-
-		} else {
-			DesiredCapabilities capability = new DesiredCapabilities();
-			capability.setBrowserName(browserName);
-			webDriver.set(new RemoteWebDriver(new URL(hubURL), capability));
-		}
 	}
 
 //	public void setDriver(RemoteWebDriver rwd) {
